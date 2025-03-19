cbd45b75ee100edce2d55d70a2cf6b73e2001838
Jessica Lasson <jessicawood4456@gmail.com>
2025-03-19 17:06:43 -0600

    Added necessary code to allow connections to the database

---
+++ backend/Scrum1314/Scrum1314/Data/Goal_Completion.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Scrum1314.Data
+{
+    public class Goal_Completion
+    {
+        [Key]
+        public int goal_completion_id { get; set; }
+
+        [ForeignKey(nameof(Goal))]
+        [Required]
+        public int goal_id { get; set; }
+        public virtual Goal? Goal { get; set; }
+
+        [Required]
+        public string date_of_completion { get; set; }
+    }
+}
